3382c60e5a8bce38f13734b254b49c35e2884054
Maksim Missuragin <mmissuragin@gmail.com>
2025-11-24 19:49:14 +0100

    первые попытки дз по модулю 6

--- src/const.tsx
+++ src/const.tsx
@@ -15,3 +15,30 @@ export enum AuthorizationStatus {
 export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
 
 export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+
+export const CITIES =[
+  {
+    id: 'paris',
+    name: 'Paris'
+  },
+  {
+    id: 'cologne',
+    name: 'Cologne'
+  },
+  {
+    id: 'brussels',
+    name: 'Brussels'
+  },
+  {
+    id: 'amsterdam',
+    name: 'Amsterdam'
+  },
+  {
+    id: 'hamburg',
+    name: 'Hamburg'
+  },
+  {
+    id: 'dusseldorf',
+    name: 'Dusseldorf'
+  },
+]
